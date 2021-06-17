8490addd26bb0d983e5554d31edea47a60027ebf
joelchrono12 <joel.chrono@disroot.org>
2021-06-17 09:38:28 -0500

    removed old scripts and scenes

--- Input.gd
+++
@@ -1,8 +0,0 @@
-extends LineEdit
-# Hello worls it is me
-func _ready() -> void:
-	grab_focus()
-
-
-func _on_Input_text_entered(_new_text: String) -> void:
-	clear()
--- RoomManager.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-
-func _ready():
-	$"Menu Principal".connect_exits("este", $"Casa Inicial")
